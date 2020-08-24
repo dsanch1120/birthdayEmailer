9c15d29479e2cb06e3f1c6a64fdab6555b7e098f
Daniel Sanchez <sanchez@isengard.mines.edu>
2020-08-24 13:11:58 -0600

    Script now sends emails to specified user

--- birthday.py
+++ birthday.py
@@ -1,9 +1,11 @@
-i Created by Daniel Sanchez
+# Created by Daniel Sanchez
 # August 24, 2020
 # Reads through .txt file and emails specified user if it is somebody's birthday
 
 #Imports datetime package to check the current date
 import datetime
+#Imports smtplib and ssl packages for sending emails
+import smtplib, ssl
 
 #Gets the current day and month
 complete_time = datetime.datetime.now()
@@ -23,8 +25,42 @@ readFile = open(FILE_NAME, 'r')
 lines = readFile.readlines()
 
 #Checks if there is a birthday today
+birthdays = []
 for i in lines:
     if i[:4] == today:
-        birthdays = [i]
+        birthdays.append(i)
+
+#Stores today's birthdays in arrays
+birthyear = []
+names = []
+for i in birthdays:
+    data = i.split("\t")
+    birthyear.append(int(data[1]))
+    names.append(data[2][:len(data[2]) - 1])
+
+#Gets ages of people with a birthday
+age = []
+for i in birthyear:
+    age.append(int(complete_time.year) - i)
+
+#Sends email to specified user with birthday information
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email        #ADD YOUR THE SENDER EMAIL ADDRESS
+reciever_email      #ADD THE RECIEVER EMAIL ADDRESS
+password = ""       #ADD THE PASSWORD TO YOUR EMAIL
+
+context = ssl.create_default_context()
+
+message = """\
+        Subject: Birthday Today!!!
+        """
+
+for i in range(len(names)):
+    message = "\n" + message + names[i] + " is " + str(age[i]) + " today! \n"
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, reciever_email, message)
 
 
